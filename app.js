57dd7f5a33331f4cfd42ef6de9328ed82a964970
Sergey Zhikharev <as1234531@yandex.ru>
2022-06-15 23:40:59 +0400

    создал структуру проекта

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require('./routes.js');
 const { PORT = 3000 } = process.env;
 const app = express();
-app.listen(PORT, () => {
 
-  console.log(`App listening on port ${PORT}`)
-})
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+app.use('/', routes)
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
