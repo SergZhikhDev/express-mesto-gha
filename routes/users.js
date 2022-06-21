c798576063a1067f7afbe184e60d9dbcfec3621d
Sergey Zhikharev <as1234531@yandex.ru>
2022-06-21 18:13:37 +0400

    add routes

--- routes/users.js
+++ routes/users.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
+
+router.get('/users', (req, res) => {
+  const { users } = req.params;
+   res.send(users)
+});
+
+router.get('/users/:id', (req, res) => {
+  res.send(req.params);
+
+    }); 
+
 module.exports = router; // экспортировали роутер
