d687e69134329b8071739b13efafa355d1e244da
Sergey Zhikharev <as1234531@yandex.ru>
2022-06-23 13:26:48 +0400

    Создал routes/users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,17 @@
-// можо удалить комментарий
+
+
+
+const User = require('../models/user');
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      }
+      return res.status(500).send({ message: 'Ошибка по умоланию' });
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,55 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/users', (req, res) => {
-  const { users } = req.params;
-   res.send(users)
+const User = require("../models/user");
+
+
+
+const {
+  getUser,
+  getUserById,
+  createUser,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/users');
+
+
+//возвращает всех пользователей
+router.get("/users", (req, res) => {
+  // const { users } = req.params;
+  // res.send(users);
+
+  User.find({})
+    // .then((user) => res.send({ data: user }))
+    .then((user) => res.status(200).send(user))//как вариант
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
 });
 
-router.get('/users/:id', (req, res) => {
-  res.send(req.params);
+//возвращает пользователя по _id
+router.get("/users/:id", (req, res) => {
+  // res.send(req.params);
+  User.findById(req.params.userid)  // app.use('/users', require('./routes/users'));
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+});
 
-    }); 
+//создаёт пользователя
+router.post('/users', createUser);
 
-module.exports = router; // экспортировали роутер
-// можо удалить комментарий
+
+// router.post("/users", (req, res) => {
+
+//   console.log('qqqqqqqqqqqqqqqqqqqqqqqqqqq', res.body)
+//   const { name, about, avatar } = req.body;
+
+
+//   User.create({ name, about, avatar })
+//     // вернём записанные в базу данные
+//     .then((user) => res.send({ data: user }))
+//     // данные не записались, вернём ошибку
+//     .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+// });
+
+module.exports = router;
+
+
+
