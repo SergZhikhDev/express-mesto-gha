2d612a2e8f89fd2101e744c6a915bfcecee0347f
Sergey Zhikharev <as1234531@yandex.ru>
2022-06-25 00:25:54 +0400

    тест

--- controllers/users.js
+++ controllers/users.js
@@ -23,58 +23,93 @@ module.exports.getUserById = (req, res) => {
       }
       return res.status(CORRECT_CODE).send(user);
     })
-    .catch((err) =>
-      res
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res
+          .status(ERROR_CODE)
+          .send({ message: "Запрашиваемый пользователь не найден" });
+      }
+      return res
         .status(SERVER_ERROR_CODE)
-        .send({ message: "Ошибка по-умолчанию" })
-    );
+        .send({ message: "Ошибка по умоланию" });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   User.create(req.body)
-  .then((user) => res.status(CORRECT_CODE).send({ data: user }))
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
-    }
-    return res.status(SERVER_ERROR_CODE).send({ message: 'Ошибка по умоланию' });
-  });
+    .then((user) => res.status(CORRECT_CODE).send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res
+          .status(ERROR_CODE)
+          .send({
+            message: "Переданы некорректные данные при создании пользователя",
+          });
+      }
+      return res
+        .status(SERVER_ERROR_CODE)
+        .send({ message: "Ошибка по умоланию" });
+    });
 };
 
 module.exports.updateProfile = (req, res) => {
-  User.findByIdAndUpdate(req.user._id, req.body, { new: true })
-  .then((user) => {
-    if (!user) {
-      return res.status(NOT_FOUND_CODE).send({ message: 'Запрашиваемый пользователь не найден' });
-    }
-    return res.status(CORRECT_CODE).send({ data: user });
-  })
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-    }
-    if (err.name === 'CastError') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-    }
-    return res.status(SERVER_ERROR_CODE).send({ message: 'Ошибка по умоланию' });
-  });
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(NOT_FOUND_CODE)
+          .send({ message: "Запрашиваемый пользователь не найден" });
+      }
+      return res.status(CORRECT_CODE).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res
+          .status(ERROR_CODE)
+          .send({
+            message: "Переданы некорректные данные при обновлении профиля",
+          });
+      }
+      if (err.name === "CastError") {
+        return res
+          .status(ERROR_CODE)
+          .send({
+            message: "Переданы некорректные данные при обновлении профиля",
+          });
+      }
+      return res
+        .status(SERVER_ERROR_CODE)
+        .send({ message: "Ошибка по умоланию" });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, req.body, { new: true })
-  .then((user) => {
-    if (!user) {
-      return res.status(NOT_FOUND_CODE).send({ message: 'Запрашиваемый пользователь не найден' });
-    }
-    return res.status(CORRECT_CODE).send({ data: user });
-  })
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
-    }
-    if (err.name === 'CastError') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
-    }
-    return res.status(SERVER_ERROR_CODE).send({ message: 'Ошибка по умоланию!' });
-  });
-}
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(NOT_FOUND_CODE)
+          .send({ message: "Запрашиваемый пользователь не найден" });
+      }
+      return res.status(CORRECT_CODE).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res
+          .status(ERROR_CODE)
+          .send({
+            message: "Переданы некорректные данные при обновлении аватара",
+          });
+      }
+      if (err.name === "CastError") {
+        return res
+          .status(ERROR_CODE)
+          .send({
+            message: "Переданы некорректные данные при обновлении аватара",
+          });
+      }
+      return res
+        .status(SERVER_ERROR_CODE)
+        .send({ message: "Ошибка по умоланию!" });
+    });
+};
