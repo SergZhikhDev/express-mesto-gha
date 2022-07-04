8480aca9c7f865800eedc56792eeda71d28c819b
Sergey Zhikharev <as1234531@yandex.ru>
2022-07-04 21:25:52 +0400

    modified:   middlewares/auth.js
    исправил 2 ошибки ревью

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,37 +1,25 @@
 const { checkToken } = require('../utils/jwt');
-
 const User = require('../models/user');
 
 const throwUnauthorizedError = () => {
   const error = new Error('Авторизуйтесь для доступа');
-
   error.statusCode = 401;
-
   throw error;
 };
 
-// eslint-disable-next-line consistent-return
-
-// eslint-disable-next-line consistent-return
 const isAuthorized = ((req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw res.status(401).send({ message: 'Необходима авторизация' });
   }
-
   const token = authorization.replace('Bearer ', '');
-
   try {
     const payload = checkToken(token);
-
-    User.findOne({ email: payload.email }).then((user) => {
+    User.findOne({ id: payload._id }).then((user) => {
       if (!user) {
         throwUnauthorizedError();
       }
-
       req.user = { id: user._id };
-
       next();
     });
   } catch (err) {
